f17c32861976256a4d32975549a92ad903ca02a1
yinzhigang12 <yinzhigang12@hotmail.com>
2017-09-11 01:09:40 +0800

    AmazeUI高级组件显示调整

---
+++ Scripts/amazeuiTouch.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+    Button,
+} from 'amazeui-touch';
+
+ReactDOM.render( < Button > Hello
+World < /Button>,document.getElementById('root'));
